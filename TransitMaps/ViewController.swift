21f5c2f8191800d01da2c271ab34f9739837054a
Elijah Kim <kimelijah90@gmail.com>
2016-10-06 14:54:03 -0400

    Show Image of NYC Transit
    * Map is pinch zoomable
    * Map is pannable
    * Rotating resized

--- TransitMaps/ViewController.swift
+++ TransitMaps/ViewController.swift
@@ -8,18 +8,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    // MARK: Attributes
+    var scrollView: UIScrollView!
+    var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        imageView = UIImageView(image: UIImage(named: "NYC"))
+        
+        scrollView = UIScrollView(frame: view.bounds)
+        scrollView.backgroundColor = UIColor.black
+        scrollView.contentSize = imageView.bounds.size
+        scrollView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        
+        scrollView.addSubview(imageView)
+        view.addSubview(scrollView)
+        
+        scrollView.delegate = self
+        setZoomScale()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillLayoutSubviews() {
+        setZoomScale()
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        let imageViewSize = imageView.frame.size
+        let scrollViewSize = scrollView.bounds.size
+        
+        let verticalPadding = imageViewSize.height < scrollViewSize.height ? (scrollViewSize.height - imageViewSize.height) / 2 : 0
+        let horizontalPadding = imageViewSize.width < scrollViewSize.width ? (scrollViewSize.width - imageViewSize.width) / 2 : 0
 
+        scrollView.contentInset = UIEdgeInsets(top: verticalPadding, left: horizontalPadding, bottom: verticalPadding, right: horizontalPadding)
 
+    }
+    
+    func setZoomScale() {
+        let imageViewSize = imageView.bounds.size
+        let scrollViewSize = scrollView.bounds.size
+        let widthScale = scrollViewSize.width / imageViewSize.width
+        let heightScale = scrollViewSize.height / imageViewSize.height
+        let minZoomScale = min(widthScale, heightScale)
+        
+        scrollView.minimumZoomScale = minZoomScale
+        scrollView.maximumZoomScale = 4.0
+        scrollView.zoomScale = minZoomScale
+    }
 }
 
